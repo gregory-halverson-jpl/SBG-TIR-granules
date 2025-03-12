f2a1ef27a857e800008f0ef2127266978de2f988
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-03-11 17:57:32 -0700

    removing MET, SM, and SEB products, need to include ETAUX

--- SBG_TIR_granules/open_granule.py
+++ SBG_TIR_granules/open_granule.py
@@ -7,10 +7,10 @@ from .granule import ECOSTRESSGranule
 from .L1CTRAD import L1CTRAD
 from .L2TLSTE import L2TLSTE
 from .L2TSTARS import L2TSTARS
+
+## FIXME include L4T ETAUX
+
 from .L4TJET import L4TJET
-from .L3TMET import L3TMET
-from .L3TSM import L3TSM
-from .L3TSEB import L3TSEB
 from .L4TESI import L4TESI
 from .L4TWUE import L4TWUE
 
@@ -35,15 +35,9 @@ def open_granule(
     elif filename_base.startswith("ECOv003_L3T_JET"):
         logger.info(f"loading Collection 2 L3T JET: {cl.file(filename)}")
         return L4TJET(filename, **kwargs)
-    elif filename_base.startswith("ECOv003_L3T_MET"):
-        logger.info(f"loading Collection 2 L3T MET: {cl.file(filename)}")
-        return L3TMET(filename, **kwargs)
-    elif filename_base.startswith("ECOv003_L3T_SM"):
-        logger.info(f"loading Collection 2 L3T SM: {cl.file(filename)}")
-        return L3TSM(filename, **kwargs)
-    elif filename_base.startswith("ECOv003_L3T_SEB"):
-        logger.info(f"loading Collection 2 L3T SEB: {cl.file(filename)}")
-        return L3TSEB(filename, **kwargs)
+    
+    ## FIXME include L4T ETAUX
+
     elif filename_base.startswith("ECOv003_L4T_ESI"):
         logger.info(f"loading Collection 2 L4T ESI: {cl.file(filename)}")
         return L4TESI(filename, **kwargs)
