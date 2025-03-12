79bb3c7a5232325bf116f37fd201b5bca1e9e263
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-03-11 17:37:45 -0700

    removing MET, SM, and SEB products, need to include ETAUX

--- SBG_TIR_granules/SBG_TIR_granules.py
+++ SBG_TIR_granules/SBG_TIR_granules.py
@@ -1,10 +1,10 @@
 from .colors import *
 from .L2TLSTE import L2TLSTE
 from .L2TSTARS import L2TSTARS
+
+## FIXME include L4T ETAUX
+
 from .L4TJET import L4TJET
-from .L3TMET import L3TMET
-from .L3TSEB import L3TSEB
-from .L3TSM import L3TSM
 from .L4TESI import L4TESI
 from .L4TWUE import L4TWUE
 from .granule import ECOSTRESSGranule
